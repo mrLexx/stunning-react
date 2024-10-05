38cdc8205967fb6377b634f7520b386dd6fdbf8a
Valery <nicemrlexx@live.ru>
2024-10-06 00:55:17 +0300

    Homework 4

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -3,40 +3,21 @@ import { Dish } from "../dish/Dish.jsx";
 
 export const Menu = ({ menu }) => {
 
-    const [moreDishes, setMoreDishes] = useState(false);
+    if (menu.length === 0) {
+        return null;
+    }
 
-    return (menu.length > 0 && (
-            <>
-                <button
-                    className={"btn btn-outline-primary btn-sm mb-2"}
-                    onClick={() => setMoreDishes(!moreDishes)}
-                >{moreDishes ? `Remove extra dishes` : `Add more dishes for progreess bar`}
-                </button>
-
-                <ul>
-                    {moreDishes ? [...Array(10)].map((x, i) => {
-                        return <Fragment key={i}>
-                            {menu.map((dish) => (
-                                <li key={dish.id}>
-                                    <Dish
-                                        name={dish.name}
-                                        ingredients={dish.ingredients}
-                                        price={dish.price}
-                                    />
-                                </li>
-                            ))}
-                        </Fragment>
-                    }) : menu.map((dish) => (
-                        <li key={dish.id}>
-                            <Dish
-                                name={dish.name}
-                                ingredients={dish.ingredients}
-                                price={dish.price}
-                            />
-                        </li>
-                    ))}
-                </ul>
-            </>
-        )
+    return (
+        <ul>
+            {menu.map((dish) => (
+                <li key={dish.id}>
+                    <Dish
+                        name={dish.name}
+                        ingredients={dish.ingredients}
+                        price={dish.price}
+                    />
+                </li>
+            ))}
+        </ul>
     );
 }
