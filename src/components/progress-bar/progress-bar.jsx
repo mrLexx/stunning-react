f3380dd38e70749736a49b39a4e28aaacbb49821
Valery <nicemrlexx@live.ru>
2024-10-06 00:44:39 +0300

    Homework 5

--- src/components/progress-bar/progress-bar.jsx
+++ src/components/progress-bar/progress-bar.jsx
@@ -1,38 +1,44 @@
-import { useEffect, useState } from "react";
+import styles from "./progress-bar.module.css"
+import { useProgressBar } from "./use-progress-bar.js";
+import classNames from "classnames";
 
-const defaultStyle = {
-    backgroundColor: '#DC2626',
-    top: 0,
-    position: 'fixed',
-    height: '10px',
-    left: 0,
-    zIndex: 9999,
-};
+export const ProgressBar = ({ viewMode="default", size="usual" }) => {
+    const { progress } = useProgressBar();
 
-export const ProgressBar = ({ style }) => {
-    const [progress, setProgress] = useState('0%');
+    const progressBarClass = [];
 
-    useEffect(() => {
-        const handleProgress = () => {
-            setProgress(Math.floor((window.scrollY * 100 / (document.body.scrollHeight - window.innerHeight))) + '%');
-        };
-
-        window.addEventListener('scroll', handleProgress);
-
-        return () => {
-            window.removeEventListener('scroll', handleProgress);
-        };
-
-
-    }, []);
+    switch (viewMode) {
+        case 'accent':
+            progressBarClass.push(styles.accent);
+            break
+        case 'default':
+        default:
+            progressBarClass.push(styles.default);
+            break
+    }
+    switch (size) {
+        case 'small':
+            progressBarClass.push(styles.sizeSmall);
+            break;
+        case 'big':
+            progressBarClass.push(styles.sizeBig);
+            break;
+        case 'usual':
+        default:
+            progressBarClass.push(styles.sizeUsual);
+            break;
+    }
 
     return (<>
         <div>
-            <div style={{
-                ...defaultStyle,
-                ...style,
-                width: progress,
-            }}></div>
+            <div
+                className={classNames(
+                    styles.progressBar,
+                    progressBarClass
+                )}
+                style={{
+                    width: progress,
+                }}></div>
         </div>
     </>);
 }
