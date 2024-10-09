b48a4e807a4dea56512961e683311e97438a967d
Valery <nicemrlexx@live.ru>
2024-10-09 12:27:48 +0300

    rename Reviews.jsx -> reviews.jsx

--- src/components/restaurant/restaurant.jsx
+++ src/components/restaurant/restaurant.jsx
@@ -1,6 +1,6 @@
 import { ReviewForm } from "../review-form/review-form.jsx";
 import { Menu } from "../menu/menu.jsx";
-import { Reviews } from "../reviews/Reviews.jsx";
+import { Reviews } from "../reviews/reviews.jsx";
 import styles from "./restaurant.module.css"
 
 export const Restaurant = ({ name, menu, reviews }) => {
