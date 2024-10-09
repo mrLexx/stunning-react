dac1b5558121e712548ac6ffa3ee02a7bbf74832
Valery <nicemrlexx@live.ru>
2024-10-09 12:21:07 +0300

    rename RestaurantsPage.jsx -> restaurants-page.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Layout } from "./components/layout/layout.jsx";
-import { RestaurantsPage } from "./components/restaurants-page/RestaurantsPage";
+import { RestaurantsPage } from "./components/restaurants-page/restaurants-page.jsx";
 
 export const App = () => {
 
