c24ca9e40b0e9b3d1ce95593f8469782de81115d
Valery <nicemrlexx@live.ru>
2024-11-29 14:42:37 +0300

    corrections

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -1,10 +1,6 @@
 import "../../index.css";
 import { App } from "./app.js";
 
-export function generateStaticParams() {
-    return [{ slug: [""] }];
-}
-
 export default function Page() {
     return <App />;
 }
